a7a856f131acf4d16221ffa69a5cf80cb81bdafc
Abel Lorin Hristodor <abelor@amazon.com>
2025-09-16 14:45:04 +0200

    fixzes

--- octofer-webhook/src/config.rs
+++ octofer-webhook/src/config.rs
@@ -1,10 +1,5 @@
 use octofer_github::GithubConfig;
 
-pub struct ServerConfig {
-    address: std::net::Ipv4Addr,
-    port: u16,
-}
-
 pub struct Config {
     gh_config: GithubConfig,
     server_config: ServerConfig,
